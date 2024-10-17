d297578c16dc0fce6fdac17ae7fbdbc11dd81970
jhnadry naula <jjnaula3@utpl.edu.ec>
2024-10-17 11:26:15 -0500

    tercer commit, trabajando con Git y GitHubClassroom

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -15,10 +15,10 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ejemplo de Java\n" +
-"Manejado desde GitHub\n" +
+        System.out.println("Ejemplo dos básico de Java\n" +
 "Versionado a través de Git\n" +
-"Creado por <ubicar su usuario de github>");
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");
     }
     
 }
